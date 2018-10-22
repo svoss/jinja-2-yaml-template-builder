b1ba0ff70da15385989933c90f093b1314e5db34
Stijn Voss <stijnvoss@Stijns-MBP.fritz.box>
2018-10-22 11:54:07 +0200

    Allowing to overwrite point separated arguments

--- src/variables.py
+++ src/variables.py
@@ -11,6 +11,18 @@ class Vars():
 		with open(fn, encoding='utf8') as io:
 			self.vars.update(load(io))
 
+	def _set_recursive_path(self, path, value):
+		path = path.split(".")
+		n = 0
+		V = self.vars
+		for p in path:
+			n += 1
+			if n == len(path):
+				V[p] = value
+			else:
+			 	V[p] = {}
+			 	V = V[p]
+			
 	def get_as_dic(self):
 		return self.vars
 
@@ -21,7 +33,7 @@ class Vars():
 			s = arg.split("=",1)
 			if len(s) < 2:
 				raise ValueError("No = sign found for argument")
-			self.vars[s[0]] = s[1]
+			self._set_recursive_path(s[0], s[1])
 
 	def load_config_files(self, folder, files):
 		if not os.path.exists(folder):
